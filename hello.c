107eb363886648a6e55d9e0b86095c1a77ec2534
Cade Covington <ccovington233@gmail.com>
2019-08-29 17:26:42 -0500

    Major
    I added the "major line"

--- hello.c
+++ hello.c
@@ -1,1 +1,16 @@
-hello.c
+/**
+ * Author: Cade Covington
+ * Date: 2019/08/29
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Cade Covington\n");
+  printf("Mechanical Engineer")
+  return 0;
+}
